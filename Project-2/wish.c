547d6368d231e2c4feef733610283559878135ea
KananaNanis <kananananis1@gmail.com>
2018-10-07 03:23:56 +0000

    added file

--- Project-2/wish.c
+++ Project-2/wish.c
@@ -4,72 +4,159 @@
 #include <errno.h>
 #include <unistd.h>
 
-#define MAXLINE 1024
 #define DELIMS " \t\r\n"
+int MAXLINE=1024;
+char prompt[]="wish> "; //command line prompt
 char *cmd;
-pid_t child_pid;
-pid_t wait_result;
-int stat_loc;
-char **command;
+int main(int argc, char **argv){
+    //buffer for fgets
+    char cmdline[MAXLINE];
+    //executing ls command
+    char* argv2[]={"ls", "-la", NULL};
+    execv(argv2[0], argv2);
 
+    while(1){
+        printf("%s", prompt);
 
-int main(int argc, char *argv[]) {
-  char *cmd;
-  char line[MAXLINE];
-  char cmdline[MAXLINE];
-  child_pid = fork();
-
-  while (1) {
-    printf("%s","wish> ");
-    
-    if (!fgets(line, MAXLINE, stdin)) break;
-
-    // Parse and execute command
-    if ((cmd = strtok(line, DELIMS))) {
-      // Clear errors
-      errno = 0;
-
-      if (strcmp(cmd, "cd") == 0) {
-
-        char *arg = strtok(0, DELIMS);
-        if (!arg) fprintf(stderr, "Missing argument.\n");
-        else chdir(arg);
-
-      } else if (strcmp(cmd, "exit") == 0) {
-        break;
-
-      }
-      if (strcmp(cmd, "path") == 1) {
-
-        char *arg = strtok(0, DELIMS);
-        chdir(arg);
-      }
-       else system(line);
-
-      if (errno) perror("Failed");
-    }
-    
-  }
-
-
-  if((fgets(cmdline, MAXLINE,stdin)==NULL) && ferror(stdin))
+        if((fgets(cmdline, MAXLINE,stdin)==NULL) && ferror(stdin))
             error("fgets error");
+
         if(feof(stdin)){
             printf("\n");
             exit(0);
         }   
+
+
+        //check if command is cd
+      if (strcmp(cmd, "cd") == 0) {
+        char *arg = strtok(0, DELIMS);
+
+        if (!arg) fprintf(stderr, "cd missing argument.\n");
+        else chdir(arg);
+      } 
+      //check if command is exit
+      else if (strcmp(cmd, "exit") == 0) break; 
+
+        //remove trailing newline
+        cmdline[strlen(cmdline)-1]='\0';
+
+        //evaluate command line
+        eval(cmdline);
+    }   
+
+
+void eval(char *cmdline){
+    int bg;
+    struct command *cmd;
+
+    printf("Evaluating '%s'\n", cmdline);
+
+    //parse cmdline into cmd structure 
+    bg=parse(cmdline, &cmd);
+    
+    if(bg==-1) return;
+    if(cmd.argv[0]==NULL) return;
+
+    if(cmd.builtin==NONE)
+        runSystemCommand(&cmd,bg);
+
+    else
+        runBuiltinCommand(&cmd,bg);    
+}
+
+#define MAXARGS 128
+struct command{
+    int argc;
+    char *argv[MAXARGS]; //argument list
+    enum builtin_t{ // is argv[0] a builtin command?
+        NONE, QUIT, JOBS, BG, FG
+    } builtin;
+
+}
+int parse(const char *cmdline, struct command *cmd){
+    static char array[MAXLINE];
+    const char delims[10]= " \t\r\n";
+    char *line =array;
+    char *token;
+    char *endline;
+    int is_bg;
+
+    if(cmdline==NULL)
+        error("Command line is null\n");
+
+    (void) strncpy(line, cmdline,MAXLINE);
+    endline =line +strlen[line];
+
+    //build argv list
+    cmd-> argc=0;
+
+    while(line<endline){
+        //skip delimeters
+        line+=strspn(line,delims);
+        if(line>=endline) break;
+
+        //find token delimeter
+        token=line+strcspn(line, delims);
+
+        //terminate the token
+        token='\0';
+
+        //record token as argument
+        cmd->argv[cmd->argc++]=line;
+
+        //check if argv is full
+        if(cmd->argv[cmd>=MAXARGS=1]) break;
+
+        line =token+1;
+    } 
+
     // The child process
    child_pid = fork();
         if (child_pid == 0) {
             /* Never returns if the call is successful */
-            execvp(command[0], command);
+            execv(command[0], command);
             printf("This won't be printed if execvp is successul\n");
         } else {
             waitpid(child_pid, &stat_loc, WUNTRACED);
         } 
-             
-  //readfile
-  FILE *readFile;
+
+    //argument list  must end with a NULL pointer
+    cmd->argv[cmd->argc]=NULL;
+
+    if(cmd->argc==0) //ignore blank line
+        return 1;
+
+    cmd->builtin=parseBuiltin(cmd);
+
+    if((in_bg=(*cmd=>argv[cmd=>argc-1]=='&')) 1=0) 
+        cmd=>argv[==cmd=>argc]=NULL;
+
+    return is_bg;          
+}
+
+void runSystemCommand(struct command *cmd, int bg){
+    pid_t childPid;
+
+    //fork process
+    if((childPid=fork())<0)
+        error("fork() error");
+    else if(childPid==0)
+        //execv
+        if(execv(cmd->argv[0],cmd->argv)<0){
+            printf("%s: Command not found\n", cmd->argv[0]);
+            exit(0);
+        }
+    else {
+        if(bg)
+            printf("Child in background [%d]\n", childPid);
+        else
+            wait(&childPid);    
+    }       
+    
+    }
+
+    //readfile
+    FILE *readFile;
     
     //check if user has entered a file name to read from
     if(argc < 2){
@@ -101,8 +188,7 @@ int main(int argc, char *argv[]) {
         fclose(readFile);  
     }
 
-  
+
+
 }
-
-
 
